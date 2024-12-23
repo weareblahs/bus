b76304252159a5f9a6853143c5776a427112cd66
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2024-12-23 12:45:09 +0800

    new data for this week (23/12-29/12)

---
+++ utils/rid_file_gen/0_download_all.bat
@@ -0,0 +1,4 @@
+@echo off
+wget https://api.data.gov.my/gtfs-static/prasarana?category=rapid-bus-penang
+wget https://api.data.gov.my/gtfs-static/prasarana?category=rapid-bus-kl
+wget https://api.data.gov.my/gtfs-static/prasarana?category=rapid-bus-kuantan
