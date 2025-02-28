679a16b0873357b18ed252ecbc80e8c74ff33b1a
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2025-02-28 10:12:28 +0800

    fix deletion of files on static time generation script, everything's back

--- utils/static_bus_time_generator/1_init.bat
+++ utils/static_bus_time_generator/1_init.bat
@@ -12,11 +12,6 @@ tar -xf prasarana@category=rapid-bus-penang -C ./penang
 tar -xf prasarana@category=rapid-bus-kl -C ./kl
 tar -xf prasarana@category=rapid-bus-kuantan -C ./kuantan
 node 2_conversion
-
-echo "Deleting existing trips file..."
-del ..\..\src\privData\trips.json -y
-echo "Replacing with new trips file..."
-move trips_generated.json ..\..\src\privData\trips.json
 echo "Deleting unused files..."
 echo "Please confirm if asked to delete directory."
 del "prasarana@category=rapid-bus-penang" -y
