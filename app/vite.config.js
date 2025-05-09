7d40a31268f8878db615423262f07159264b0f20
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2025-05-09 21:01:40 +0800

    change some axios parts to ky, update all packages

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,9 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: ["gtfs-realtime-bindings"],
-    },
-  },
+  build: {},
 });
