9a203076a6425168dd2d651e211734d8d9dfad76
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2025-05-09 21:03:50 +0800

    externalize non-vite module

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {},
+  build: {
+    rollupOptions: {
+      external: ["gtfs-realtime-bindings", "axios"],
+    },
+  },
 });
