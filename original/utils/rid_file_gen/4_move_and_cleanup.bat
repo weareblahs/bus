ebd832d78d24d83926fdced38a8a10ac82405059
Tan Yu Xuan <tanyuxuan2005@gmail.com>
2025-09-30 10:07:18 +0800

    remove duplicate utils, add line to copy trips.json to bus v2, data update 30/9/2025

--- original/utils/rid_file_gen/4_move_and_cleanup.bat
+++ original/utils/rid_file_gen/4_move_and_cleanup.bat
@@ -3,6 +3,8 @@ echo "Deleting existing trips file..."
 del ..\..\src\privData\trips.json -y
 echo "Replacing with new trips file..."
 move trips_generated.json ..\..\src\privData\trips.json
+echo "Copying files to v2..."
+copy ..\..\src\privData\trips.json ..\..\..\app\src\internalData\trips.json
 echo "Deleting unused files..."
 echo "Please confirm if asked to delete directory."
 del "prasarana@category=rapid-bus-penang" -y
