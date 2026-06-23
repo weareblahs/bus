433bfd7cebd9ac810d7738de27679172e721898c
nottanyuxuan <tanyuxuan2005@gmail.com>
2026-06-23 14:27:39 +0800

    fix: 🐛 ...wait, the bas.my generation file ACTUALLY fixes rpg?
    overwrite rapidpenang file generation script with script from bas.my and
    it works as expected

--- utils/initStnData_rpg.js
+++ utils/initStnData_rpg.js
@@ -1,185 +1,169 @@
-// SPECIFIC FOR RAPID KL MRT FEEDER DATA ONLY
-// trip_headsign in trips = route name
-// possibly using a similar system to rapid penang
-
 import fs from "fs";
 import csvParser from "csv-parser";
 
-const routes = [];
-const stopTimes = [];
-const stops = [];
-const trips = [];
+const stationsToGenerate = ["rapidpenang"];
 
-// parse stopTimes
-await new Promise((resolve, reject) => {
-  fs.createReadStream("rapidpenang/routes.txt")
-    .pipe(csvParser())
-    .on("data", (row) => {
-      routes.push(row);
-    })
-    .on("end", resolve)
-    .on("error", reject);
-});
+const exportDir = ["rapidpenang"];
 
-// parse routes
-await new Promise((resolve, reject) => {
-  fs.createReadStream("rapidpenang/stop_times.txt")
-    .pipe(csvParser())
-    .on("data", (row) => {
-      stopTimes.push(row);
-    })
-    .on("end", resolve)
-    .on("error", reject);
-});
+// create blank foreach from stationsToGenerate with index
+stationsToGenerate.forEach(async (station, index) => {
+  const targetDir = exportDir[index];
 
-// parse stops
-await new Promise((resolve, reject) => {
-  fs.createReadStream("rapidpenang/stops.txt")
-    .pipe(csvParser())
-    .on("data", (row) => {
-      stops.push(row);
-    })
-    .on("end", resolve)
-    .on("error", reject);
-});
+  const routes = [];
+  const stopTimes = [];
+  const stops = [];
+  const trips = [];
 
-await new Promise((resolve, reject) => {
-  fs.createReadStream("rapidpenang/trips.txt")
-    .pipe(csvParser())
-    .on("data", (row) => {
-      trips.push(row);
-    })
-    .on("end", resolve)
-    .on("error", reject);
-});
+  // parse stopTimes
+  await new Promise((resolve, reject) => {
+    fs.createReadStream(`${station}/routes.txt`)
+      .pipe(csvParser())
+      .on("data", (row) => {
+        routes.push(row);
+      })
+      .on("end", resolve)
+      .on("error", reject);
+  });
 
-// generate station data
-const stations = [];
-stops.forEach((s) => {
-  stations.push({
+  // parse routes
+  await new Promise((resolve, reject) => {
+    fs.createReadStream(`${station}/stop_times.txt`)
+      .pipe(csvParser())
+      .on("data", (row) => {
+        stopTimes.push(row);
+      })
+      .on("end", resolve)
+      .on("error", reject);
+  });
+
+  // parse stops
+  await new Promise((resolve, reject) => {
+    fs.createReadStream(`${station}/stops.txt`)
+      .pipe(csvParser())
+      .on("data", (row) => {
+        stops.push(row);
+      })
+      .on("end", resolve)
+      .on("error", reject);
+  });
+
+  await new Promise((resolve, reject) => {
+    fs.createReadStream(`${station}/trips.txt`)
+      .pipe(csvParser())
+      .on("data", (row) => {
+        trips.push(row);
+      })
+      .on("end", resolve)
+      .on("error", reject);
+  });
+
+  const rteData = routes.map((r) => {
+    const compiledInfo = {
+      routeId: r.route_id,
+      routeName: r.route_short_name,
+      routeShortName: r.route_long_name,
+      routeColor: r.route_color,
+      routeTextColor: r.route_text_color,
+      routeStations: [],
+      routeStationsRev: [],
+    };
+
+    // get route station sequence (fwd)
+    const foundTrip = trips.find(
+      (tr) => tr.route_id === r.route_id && tr.direction_id === "0",
+    );
+
+    const foundTripRev = trips.find(
+      (tr) => tr.route_id === r.route_id && tr.direction_id === "1",
+    );
+    if (foundTrip && foundTrip.length !== 0) {
+      // parse from stoptimes
+      const stopTime = stopTimes.filter(
+        (st) => st.trip_id === foundTrip.trip_id,
+      );
+      stopTime.forEach((st) => {
+        compiledInfo.routeStations[parseInt(st.stop_sequence) - 1] = st.stop_id;
+      });
+    }
+
+    if (foundTripRev && foundTripRev.length !== 0) {
+      // parse from stoptimes
+      const stopTime = stopTimes.filter(
+        (st) => st.trip_id === foundTripRev.trip_id,
+      );
+      stopTime.forEach((st) => {
+        compiledInfo.routeStationsRev[parseInt(st.stop_sequence) - 1] =
+          st.stop_id;
+      });
+    }
+
+    // fallback
+    if (
+      compiledInfo.routeStations.length === 0 &&
+      compiledInfo.routeStationsRev.length > 0
+    ) {
+      compiledInfo.routeStations = compiledInfo.routeStationsRev;
+      compiledInfo.routeStationsRev = [];
+    }
+    return compiledInfo;
+  });
+
+  console.log(`Total found routes: ${rteData.length}`);
+
+  // write routes.json
+  fs.writeFileSync(
+    `../public/${targetDir}/routes.json`,
+    JSON.stringify(rteData),
+  );
+
+  // get station info
+  const stopsFile = stops.map((s) => ({
     id: s.stop_id,
     name: s.stop_name,
-    desc: s.stop_desc,
-    lat: parseFloat(s.stop_lat),
-    lon: parseFloat(s.stop_lon),
-  });
-});
+    lat: s.stop_lat,
+    lon: s.stop_lon,
+  }));
 
-// lat_alt and lon_alt
-const filteredStationNames = [...new Set(stations.map((st) => st.name))];
-filteredStationNames.forEach((fsn) => {
-  const similarStnName = stations.filter((st) => st.name === fsn);
-  if (similarStnName.length > 1) {
-    // placeholder: actual alt assignment happens after routes (file) is generated
-  }
-});
+  console.log(`Total found stations: ${stopsFile.length}`);
 
-const file = [];
-
-// generate routes data
-routes.forEach((r) => {
-  let initialData = {
-    routeId: r.route_id, // route id used for parsing
-    routeName: r.route_long_name, // route id (usually route number)
-    routeShortName: "", // general name for route
-    // NOTE: route colors are unavailable
-    // The following route colors are loosely based off the bus display
-    routeColor: "19499F", // styling: bg color
-    routeTextColor: "FF0D00", // styling: fg color
-  };
-  initialData.routeShortName = trips.find(
-    (t) => t.route_id === initialData.routeId,
-  ).trip_headsign;
-
-  // generate routeStations
-  let stn = [];
-  const firstTripId = trips.find((t) => t.route_id === r.route_id).trip_id;
-  const tripIdStn = stopTimes.filter((st) => st.trip_id === firstTripId);
-  tripIdStn.forEach((f) => {
-    stn[parseInt(f.stop_sequence) - 1] = f.stop_id;
-  });
-
-  // generate reverse route stations
-  const rev0 = [];
-  const rev1 = [];
-  let rev = [];
-
-  const firstFoundRevTripId = trips.find(
-    (t) => t.route_id === r.route_id && t.direction_id === "0",
+  // write stations.json
+  fs.writeFileSync(
+    `../public/${targetDir}/stations.json`,
+    JSON.stringify(stopsFile),
   );
-  if (firstFoundRevTripId) {
-    const tid = firstFoundRevTripId.trip_id;
-    const revTIDstn = stopTimes.filter((st) => st.trip_id === tid);
 
-    revTIDstn.forEach((f) => {
-      rev0[parseInt(f.stop_sequence) - 1] = f.stop_id;
-    });
-  }
-
-  const firstFoundRevTripId1 = trips.find(
-    (t) => t.route_id === r.route_id && t.direction_id === "1",
-  );
-  if (firstFoundRevTripId1) {
-    const tid = firstFoundRevTripId1.trip_id;
-    const revTIDstn = stopTimes.filter((st) => st.trip_id === tid);
-
-    revTIDstn.forEach((f) => {
-      rev1[parseInt(f.stop_sequence) - 1] = f.stop_id;
-    });
-  }
-
-  // if both have data then begin comparision
-  if (rev0 && rev1) {
-    if (rev0 !== stn) {
-      rev = rev1;
-    } else if (rev1 !== stn) {
-      rev = rev0;
+  // create associated (fwd/rev?)
+  // relatedRoutes and relatedRoutesAlt
+  const did_fwd = {};
+  const did_rev = {};
+  //   trips.route_id (direction_id)
+  trips.forEach((t) => {
+    if (t.direction_id === "1") {
+      if (did_rev[t.route_id]) {
+        did_rev[t.route_id].push(t.trip_id);
+      } else {
+        did_rev[t.route_id] = [t.trip_id];
+      }
+    } else {
+      if (did_fwd[t.route_id]) {
+        did_fwd[t.route_id].push(t.trip_id);
+      } else {
+        did_fwd[t.route_id] = [t.trip_id];
+      }
     }
-  }
-
-  file.push({
-    ...initialData,
-    routeStations: stn,
-    routeStationsRev: JSON.stringify(rev) !== JSON.stringify(stn) ? rev : [],
   });
+
+  console.log("writing relatedRoutes...");
+  fs.writeFileSync(
+    `../public/${targetDir}/relatedRoutes.json`,
+    JSON.stringify(did_fwd),
+  );
+  console.log("writing relatedRoutesAlt...");
+  fs.writeFileSync(
+    `../public/${targetDir}/relatedRoutesAlt.json`,
+    JSON.stringify(did_rev),
+  );
+  console.log(
+    `Files for ${station} (output folder: public/${targetDir}) has been generated\n`,
+  );
 });
-
-// write routes.json
-fs.writeFileSync("../public/rapidpenang/routes.json", JSON.stringify(file));
-
-fs.writeFileSync(
-  "../public/rapidpenang/stations.json",
-  JSON.stringify(stations),
-);
-
-console.log(`Total found routes: ${file.length}`);
-console.log(`Total found stations: ${stations.length}`);
-
-// generate trips file
-const tripsFound = {};
-const tripsFoundAlt = {};
-// generates multiple files for trips and alternative trips if have
-
-trips.forEach((t) => {
-  // using the same logic above, check if the position is expected
-  if (t.direction_id === "1") {
-    tripsFoundAlt[t.route_id] = tripsFoundAlt[t.route_id] || [];
-    tripsFoundAlt[t.route_id].push(t.trip_id);
-  } else {
-    tripsFound[t.route_id] = tripsFound[t.route_id] || [];
-    tripsFound[t.route_id].push(t.trip_id);
-  }
-});
-
-console.log("Generation complete!");
-console.log("Writing to relatedRoutes.json...");
-fs.writeFileSync(
-  "../public/rapidpenang/relatedRoutes.json",
-  JSON.stringify(tripsFound),
-);
-
-console.log("Writing to relatedRoutesAlt.json...");
-fs.writeFileSync(
-  "../public/rapidpenang/relatedRoutesAlt.json",
-  JSON.stringify(tripsFoundAlt),
-);
