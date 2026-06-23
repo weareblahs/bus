c65a287ae6462ca4fecb6a4313b3e14dac9b8dc2
nottanyuxuan <tanyuxuan2005@gmail.com>
2026-06-23 13:49:19 +0800

    feat: 🎸 bas.my routes.json / stations.json generation complete

--- utils/basdotmy_network/initStnData.js
+++ utils/basdotmy_network/initStnData.js
@@ -1,9 +1,32 @@
 import fs from "fs";
 import csvParser from "csv-parser";
+import { compile } from "tailwindcss";
 
-const stationsToGenerate = ["mybas-alor-setar"];
+const stationsToGenerate = [
+  "mybas-alor-setar",
+  "mybas-ipoh",
+  "mybas-johor",
+  "mybas-kangar",
+  "mybas-kota-bharu",
+  "mybas-kuala-terengganu",
+  "mybas-kuching",
+  "mybas-melaka",
+  "mybas-seremban-a",
+  "mybas-seremban-b",
+];
 
-const exportDir = ["mybasalorsetar"];
+const exportDir = [
+  "mybasalorsetar",
+  "mybasipoh",
+  "mybasjb",
+  "mybaskangar",
+  "mybaskb",
+  "mybaskt",
+  "mybaskuching",
+  "mybasmelaka",
+  "mybasseremban",
+  "mybasserembanb",
+];
 
 // create blank foreach from stationsToGenerate with index
 stationsToGenerate.forEach(async (station, index) => {
@@ -57,6 +80,65 @@ stationsToGenerate.forEach(async (station, index) => {
       .on("error", reject);
   });
 
+  const rteData = routes.map((r) => {
+    const compiledInfo = {
+      routeId: r.route_id,
+      routeName: r.route_short_name,
+      routeShortName: r.route_long_name,
+      routeColor: r.route_color,
+      routeTextColor: r.route_text_color,
+      routeStations: [],
+      routeStationsRev: [],
+    };
+
+    // get route station sequence (fwd)
+    const foundTrip = trips.find(
+      (tr) => tr.route_id === r.route_id && tr.direction_id === "0",
+    );
+
+    const foundTripRev = trips.find(
+      (tr) => tr.route_id === r.route_id && tr.direction_id === "1",
+    );
+    if (foundTrip && foundTrip.length !== 0) {
+      // parse from stoptimes
+      const stopTime = stopTimes.filter(
+        (st) => st.trip_id === foundTrip.trip_id,
+      );
+      stopTime.forEach((st) => {
+        compiledInfo.routeStations[parseInt(st.stop_sequence) - 1] = st.stop_id;
+      });
+    }
+
+    if (foundTripRev && foundTripRev.length !== 0) {
+      // parse from stoptimes
+      const stopTime = stopTimes.filter(
+        (st) => st.trip_id === foundTripRev.trip_id,
+      );
+      stopTime.forEach((st) => {
+        compiledInfo.routeStationsRev[parseInt(st.stop_sequence) - 1] =
+          st.stop_id;
+      });
+    }
+
+    // fallback
+    if (
+      compiledInfo.routeStations.length === 0 &&
+      compiledInfo.routeStationsRev.length > 0
+    ) {
+      compiledInfo.routeStations = compiledInfo.routeStationsRev;
+      compiledInfo.routeStationsRev = [];
+    }
+    return compiledInfo;
+  });
+
+  console.log(`Total found routes: ${rteData.length}`);
+
+  // write routes.json
+  fs.writeFileSync(
+    `../../public/${targetDir}/routes.json`,
+    JSON.stringify(rteData),
+  );
+
   // get station info
   const stopsFile = stops.map((s) => ({
     id: s.stop_id,
@@ -75,31 +157,4 @@ stationsToGenerate.forEach(async (station, index) => {
 
   // get stops info
   // get general basic info
-  // sample data
-  ///  {
-  //     "routeId": "30000019",
-  //     "routeName": "401",
-  //     "routeShortName": "JETI - TELUK KUMBAR",
-  //     "routeColor": "19499F",
-  //     "routeTextColor": "FF0D00",
-  //     "routeStations": [
-  //     ],
-  //     "routeStationsRev": [
-  //     ]
-  //   },
-  let rteData = [];
-
-  routes.forEach((r) => {
-    const compiledInfo = {
-      routeId: r.route_id,
-      routeName: r.route_short_name,
-      routeShortName: r.route_long_name,
-      routeColor: r.route_color,
-      routeTextColor: r.route_text_color,
-      routeStations: [],
-      routeStationsRev: [],
-    };
-
-    // get route stations
-  });
 });
